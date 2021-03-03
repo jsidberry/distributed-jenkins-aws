7ce686b95e6c3ce4ff0475a614fe276a6d65eb2e
Juan Sidberry <jsidberry@gmail.com>
2021-03-03 16:56:22 -0500

    fix comments

--- instances.tf
+++ instances.tf
@@ -10,8 +10,8 @@ data "aws_ssm_parameter" "linuxAmiOregon" {
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-#Please note that this code expects SSH key pair to exist in default dir under 
-#users home directory, otherwise it will fail
+#This code expects SSH key pair to exist in default directory 
+#otherwise it will fail
 
 #Create key-pair for logging into EC2 in us-east-2
 resource "aws_key_pair" "master-key" {
@@ -58,6 +58,7 @@ resource "aws_instance" "jenkins-worker-oregon" {
   tags = {
     Name = join("_", ["jenkins_worker_tf", count.index + 1])
   }
+  
   depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc, aws_instance.jenkins-master]
 }
 
