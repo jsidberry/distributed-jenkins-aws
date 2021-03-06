978a3cf96f5c67ce97bc73545e2ce3ba939fcef4
Juan Sidberry <jsidberry@gmail.com>
2021-03-06 11:37:40 -0500

    add https redirect functionality

--- alb.tf
+++ alb.tf
@@ -32,14 +32,33 @@ resource "aws_lb_target_group" "app-lb-tg" {
   }
 }
 
+#Create listener on tcp/443 HTTPS
+resource "aws_lb_listener" "jenkins-listener-https" {
+  provider          = aws.region_master
+  load_balancer_arn = aws_lb.application-lb.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-lb-tg.arn
+  }
+}
+
+# Create a permanent redirect of all tcp/80-HTTP traffic to tcp/443-HTTPS
 resource "aws_lb_listener" "jenkins-listener-http" {
   provider          = aws.region_master
   load_balancer_arn = aws_lb.application-lb.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.id
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
